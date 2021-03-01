1257b99424e1bd6065e6ce2d89e47365f50172c2
walker laury <walkerclaury@protonmail.com>
2021-03-01 05:03:51 -0600

    Created navbar, needs styles and bootstrap, started on scrolling element

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <a href="#">The Great Sig Eatery</a>
+        </nav>
+    )
+}
